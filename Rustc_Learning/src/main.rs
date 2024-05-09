bb94136d03cfd170cc33a14bbbc8201302ef6a44
himanshumistri <mistrihimanshu@gmail.com>
2024-05-09 12:32:03 +0530

    1.Added how to define const in Rust and how to print it. 2.Added another way to println! in greet method
    Signed-off-by: himanshumistri <mistrihimanshu@gmail.com>

--- Rustc_Learning/src/main.rs
+++ Rustc_Learning/src/main.rs
@@ -1,3 +1,7 @@
+
+//Here is how you can define const in Rust program
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3; 
+
 fn main() {
     println!("Hello, world!");
     let myName =String::from("Himanshu Mistri");
@@ -9,5 +13,10 @@ fn main() {
 }
 
 fn greet(name: &str,age:&u32) {
+    //
     println!("Hello, {}! I am {}! year old", name,age);
+    //Here is another way to print same output as well
+    println!("Hello, {name}! I am {age}! year old");
+    //Print const value here
+    println!("My value is from const, 3 hour contain {THREE_HOURS_IN_SECONDS} seconds");
 }
